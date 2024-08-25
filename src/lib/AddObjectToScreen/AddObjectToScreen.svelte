9599755103cd82bfc4dc3913ff423a429c6ff218
chinmay@wsl <>
2024-08-25 17:33:37 +0530

    config  alias for src/lib

---
+++ src/lib/AddObjectToScreen/AddObjectToScreen.svelte
@@ -0,0 +1,22 @@
+<script lang="ts"></script>
+
+<form>
+  <label>
+    <span>X position</span>
+    <input type="number" />
+  </label>
+  <label>
+    <span>Y position</span>
+    <input type="number" />
+  </label>
+  <label>
+    <span>width</span>
+    <input type="number" />
+  </label>
+  <label>
+    <span>height</span>
+    <input type="number" />
+  </label>
+</form>
+
+<style></style>
